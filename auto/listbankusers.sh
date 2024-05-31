433c3613ce31336a03d3943bf07e06f7873fd6f1
keithpuzey <keithpuzey@users.noreply.github.com>
2024-05-31 14:52:55 +0100

    Remove Debug

--- auto/listbankusers.sh
+++ auto/listbankusers.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Debugging: Print current directory and list files
-echo "Current directory: $(pwd)"
-echo "Listing files:"
-ls -la ./auto
-
 clear
 mysql -h 10.128.0.118 -u root -pCAdemo123 -P 3309 < ./auto/digitalbank.sql
