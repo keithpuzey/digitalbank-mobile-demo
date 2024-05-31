bb37daf985c56c38e1bd634cfc2b89b234e7de8e
keithpuzey <keithpuzey@users.noreply.github.com>
2024-05-31 14:41:42 +0100

    fix dbank user

--- auto/listbankusers.sh
+++ auto/listbankusers.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+
+# Debugging: Print current directory and list files
+echo "Current directory: $(pwd)"
+echo "Listing files:"
+ls -la ./auto
+
 clear
-mysql -h 10.128.0.118 -u root -pCAdemo123 -P 3309 < digitalbank.sql
+mysql -h 10.128.0.118 -u root -pCAdemo123 -P 3309 < .auto/digitalbank.sql
