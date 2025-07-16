3badd415a634b492cbb5710c3a6024bba8bd1714
keith Puzey <keithpuzey@users.noreply.github.com>
2025-07-16 13:09:12 +0100

    Add API Monitoring Test Trigger

--- auto/config.py
+++ auto/config.py
@@ -1,17 +1,13 @@
 import os
 
-# Get BlazeMeter token from environment
-token_BMCredentials = os.getenv("BMCredentials")
-if not token_BMCredentials:
-   raise RuntimeError("❌ Environment variable 'BMCredentials' is not set.")
-# Use the token as needed
-BMCredentials = token_BMCredentials
+# Get tokens from Jenkins environment
+BMCredentials = os.getenv("BMCredentials")
+if not BMCredentials:
+    raise RuntimeError("❌ Environment variable 'BMCredentials' is not set.")
 
 PerfectoKey = os.getenv("PerfectoToken")
-if not token_BMCredentials:
-   raise RuntimeError("❌ Environment variable 'PerfectoToken' is not set.")
-# Use the token as needed
-PerfectoKey = token_PerfectoToken
+if not PerfectoKey:
+    raise RuntimeError("❌ Environment variable 'PerfectoToken' is not set.")
 
 # Blazemeter Environment - Workspace / Account
 workspaceID = 2014117
@@ -24,34 +20,7 @@ TemplateID = "6183"
 SharedFolderID = "65d71b4e8b2044fe570baf89"
 RegistrationMockID = "144333"
 
-
-# Blazemeter Environment - Workspace / Account
-workspaceID = 2014117
-account = 352831
-
-# Blazemeter Mock Service Details
-ServiceID = "338990"
-MockThinkTime = "60"
-TemplateID = "6183"
-SharedFolderID = "65d71b4e8b2044fe570baf89"
-RegistrationMockID = "144333"
-
-BMCredentials = (
-    token_BMCredentials )
-
-PerfectoKey = "token_perfectotoken"
-
-base_dir = '/var/lib/jenkins/workspace/DBank Mobile Pipeline/auto/results/'
-mock_output = f'{base_dir}mockurl.txt'
-mock_output_id = f'{base_dir}mockid.txt'
-test_result_url = f'{base_dir}test_result.txt'
-test_data_csv = f'{base_dir}test_data.csv'
-
-BMCredentials = (
-    token_BMCredentials )
-
-PerfectoKey = "token_perfectotoken"
-
+# Result paths
 base_dir = '/var/lib/jenkins/workspace/DBank Mobile Pipeline/auto/results/'
 mock_output = f'{base_dir}mockurl.txt'
 mock_output_id = f'{base_dir}mockid.txt'
